4110686a7db486707c547ceacb3c71d8cee04f59
Sanjeevani Kandra <sanjeevanikandra@gmail.com>
2025-01-15 22:30:58 +0530

    update api call

--- server/models/todo.js
+++ server/models/todo.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 // Create a schema
 const todoSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  priority: { type: String, enum: ['High', 'Medium', 'Low'], default: 'Medium' },
+  priority: { type: String, enum: ['High', 'Medium', 'Low'], default: 'Low' },
   createdAt: { type: Date, default: Date.now },
   isCompleted: { type: Boolean, default: false }
 });
